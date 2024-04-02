81f3b37c048ab6054ffaaa19b905686c5717fc78
parkdragonstone <pys9610@gmail.com>
2024-04-02 18:20:46 +0900

    first commit

---
+++ data_concat.py
@@ -0,0 +1,43 @@
+def data_concat():
+    import numpy as np
+    from glob import glob
+    import pandas as pd
+
+    KINEMATIC_PATH = 'data/**/kine/*csv'
+    FORCE_PATH = 'data/**/force/*csv'
+
+    KINEMATIC_DIR = [i.replace('\\','/') for i in glob(KINEMATIC_PATH)]
+    FORCE_DIR = [i.replace('\\','/') for i in glob(FORCE_PATH)]
+
+    kdf = pd.DataFrame()
+    fdf = pd.DataFrame()
+
+    kdf = pd.DataFrame()
+    fdf = pd.DataFrame()
+
+    for kine_dir, force_dir in zip(KINEMATIC_DIR, FORCE_DIR):
+        kine = pd.read_csv(kine_dir)
+        force = pd.read_csv(force_dir)
+        
+        _, kday, _, kfname = kine_dir.split('/')
+        _, fday, _, ffname = force_dir.split('/')
+        kfname = kfname.replace('.csv','')
+        kplayer_name, ktrial, _, _, _, _, kball, _ = kfname.split('_')
+        
+        ffname = ffname.replace('.csv','')
+        fplayer_name, ftrial, _, _, _, _, fball, _ = ffname.split('_')
+        
+        
+        kine['player'] = kplayer_name
+        kine['day'] = kday
+        kine['trial'] = ktrial
+        kine['ball_speed'] = kball
+        
+        force['player'] = fplayer_name
+        force['day'] = fday
+        force['trial'] = ftrial
+        force['ball_speed'] = fball
+        
+        kdf = pd.concat([kdf, kine])
+        fdf = pd.concat([fdf, force])
+    return kdf, fdf
--- pitching_dashboard.py
+++ pitching_dashboard.py
@@ -4,41 +4,9 @@ import plotly.graph_objs as go
 import streamlit as st
 import numpy as np
 from glob import glob
+import data_concat
 
-KINEMATIC_PATH = 'data/**/kine/*csv'
-FORCE_PATH = 'data/**/force/*csv'
-
-KINEMATIC_DIR = [i.replace('\\','/') for i in glob(KINEMATIC_PATH)]
-FORCE_DIR = [i.replace('\\','/') for i in glob(FORCE_PATH)]
-
-kdf = pd.DataFrame()
-fdf = pd.DataFrame()
-
-for kine_dir, force_dir in zip(KINEMATIC_DIR, FORCE_DIR):
-    kine = pd.read_csv(kine_dir)
-    force = pd.read_csv(force_dir)
-    
-    _, kday, _, kfname = kine_dir.split('/')
-    _, fday, _, ffname = force_dir.split('/')
-    kfname = kfname.replace('.csv','')
-    kplayer_name, ktrial, _, _, _, _, kball, _ = kfname.split('_')
-    
-    ffname = ffname.replace('.csv','')
-    fplayer_name, ftrial, _, _, _, _, fball, _ = ffname.split('_')
-    
-    
-    kine['player'] = kplayer_name
-    kine['day'] = kday
-    kine['trial'] = ktrial
-    kine['ball_speed'] = kball
-    
-    force['player'] = fplayer_name
-    force['day'] = fday
-    force['trial'] = ftrial
-    force['ball_speed'] = fball
-    
-    kdf = pd.concat([kdf, kine])
-    fdf = pd.concat([fdf, force])
+kdf, fdf = data_concat.data_concat()
 
 st.set_page_config(layout="wide")
 
